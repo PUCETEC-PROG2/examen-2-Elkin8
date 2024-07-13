4c8aabe8bc7060df99b019460c3fdde5f9b300e1
Alee234324444444444444444444 <apbarrionuevo@puce.edu.ec>
2024-07-13 13:21:50 -0500

    Examen 2

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    name = models.CharField(max_length=30, null=False)
+    gender = models.CharField(max_length=30, null=False)
+    director = models.CharField(max_length=45, null=False)
+    publication= models.CharField(max_length=30)
+    synopsis = models.TextField(max_length=800, null=False)
+    
+    def __str__(self) -> str:
+        return self.name
